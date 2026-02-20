7ec2b37c5b4f66a16668e549948c84e802f99d36
Incipient <incipient@Incipients-MacBook-Air.local>
2026-02-19 21:10:15 -0500

    Hide scenario titles during gameplay and list - show dispatch text only, no spoilers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ems-trainer-v12';
+const CACHE_NAME = 'ems-trainer-v13';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
