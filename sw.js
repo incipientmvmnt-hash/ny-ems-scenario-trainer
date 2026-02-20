f30e117bcdc108117f6e3e995ab1b8c3c82a6c43
Incipient <incipient@Incipients-MacBook-Air.local>
2026-02-19 20:27:10 -0500

    Remove ALS arrives references from ALS-level scenarios - Paramedics ARE ALS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ems-trainer-v10';
+const CACHE_NAME = 'ems-trainer-v11';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
